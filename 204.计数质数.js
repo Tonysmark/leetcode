b4c341e8da40cdef24d08491908ff5a3ee64c354
GaoNing <mjyuyu@qq.com>
2020-01-01 19:07:02 +0800

    添加代码标准化文件

--- 204.计数质数.js
+++ 204.计数质数.js
@@ -10,13 +10,13 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-	const arr_prime = [];
-	for (let i = 2; i < n; i++) {
-		if (i % 2 !== 0) {
-			arr_prime.push(i);
-		}
-	}
-	return arr_prime;
+    const arr_prime = [];
+    for (let i = 2; i < n; i++) {
+        if (i % 2 !== 0) {
+            arr_prime.push(i);
+        }
+    }
+    return arr_prime;
 };
 console.log('countPrimes() : ', countPrimes(10));
 // @lc code=end
